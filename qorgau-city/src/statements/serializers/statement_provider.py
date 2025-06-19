54ae11598999a9f694728f42d552176ac1ea1d54
Bekkhan <87779571856b@gmail.com>
2025-06-19 09:05:46 +0500

    fixes

--- qorgau-city/src/statements/serializers/statement_provider.py
+++ qorgau-city/src/statements/serializers/statement_provider.py
@@ -51,9 +51,9 @@ class StatementProviderSerializer(serializers.ModelSerializer):
         statement = validated_data.get('statement')
         provider = validated_data.get('provider')
 
-        # Check if statement exists and is not archived
-        if statement.status == auths.Status.ARCHIVED:
-            raise ValidationError("Нельзя добавить поставщика к архивированному заявлению.")
+        # Check if statement exists and is active
+        if not statement.is_active:
+            raise ValidationError("Нельзя добавить поставщика к неактивной заявке.")
 
         with transaction.atomic():
             statement_provider = StatementProvider.objects.create(**validated_data)
